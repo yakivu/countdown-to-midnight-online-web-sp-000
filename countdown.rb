2bad3dac5683dacfa455f2e4fe7ac43ae130498f
Adam Walker <adamw207@yahoo.com>
2020-02-20 14:23:12 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
-end
+  def countdown(number)
+    number = (11-1)
+    while number < 11 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    break if number == 0 
+  end
+  def countdown_with_sleep
+    sleep(5)
+  end
+
+
+puts "HAPPY NEW YEAR"
+end 
+
+countdown
+
