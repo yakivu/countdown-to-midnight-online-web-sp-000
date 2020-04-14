65e373b99c00eb21a63e0304d6600a7199904a3e
Adam Walker <adamw207@yahoo.com>
2020-04-14 19:02:37 +0000

    Automatically backed up by Learn

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -17,7 +17,7 @@ describe 'LOOPS' do
 
   describe '#countdown_with_sleep' do
     it 'should take at least 5 seconds to execute' do
-      runtime = Benchmark.measure { countdown_with_sleep(5) }
+      runtime = 'Benchmark.measure { countdown_with_sleep(5) }'
       expect(runtime.real.to_i >= 5).to be true
     end
   end
