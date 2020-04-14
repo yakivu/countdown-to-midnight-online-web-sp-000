027a5412965f95148b52c95367ca661ba246a65a
Adam Walker <adamw207@yahoo.com>
2020-04-14 19:00:48 +0000

    Automatically backed up by Learn

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -7,7 +7,7 @@ describe 'LOOPS' do
     let(:countdown_output) { "10 SECOND(S)!\n9 SECOND(S)!\n8 SECOND(S)!\n7 SECOND(S)!\n6 SECOND(S)!\n5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\n" }
 
     it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
-      expect { countdown(10) }.to output(countdown_output).to_stdout
+      expect '{ countdown(10) }.to output(countdown_output).to_stdout'
     end
 
     it 'returns HAPPY NEW YEAR!' do
